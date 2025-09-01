83688f6e6fbbb1474065a18c63387d6dab1707d9
Shreyash Magdum <shreyashmagdum1215@gmail.com>
2025-09-01 16:52:13 +0530

    1-9-25

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,27 @@
 import RestaurantCard from "./RestaurantCard"
 import restaurantsList from "../utils/Reslist"
+import { useState } from "react"
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantsList)
+
   return (
     <div className="body">
       <div className="rating-button">
-        <button className="top-rated" onClick={() => {}}>
+        <button
+          className="top-rated"
+          onClick={() => {
+            const filterList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            )
+            setListOfRestaurants(filterList)
+          }}
+        >
           Top Rated
         </button>
       </div>
       <div className="res-container">
-        {restaurantsList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
