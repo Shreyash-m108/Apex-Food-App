c975fd7fd1d4fcdfd7c59ce88d7763a235b6acb3
Shreyash <shreyashmagdum1215@gmail.com>
2025-11-12 19:46:52 +0530

    12-11

---
+++ dist/Grocery.7dab3fe3.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire60b4;(0,e.register)("63M6l",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0"),a=()=>(0,t.jsx)("div",{children:"Get grocery and other items in a 10mins"})});
+//# sourceMappingURL=Grocery.7dab3fe3.js.map
